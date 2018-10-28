754119f79c38dc95329a44ca3435133b5e6ec941
rerost <gmailii@yahoo.co.jp>
2018-10-29 00:07:01 +0900

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_28_150600) do
+
+  create_table "users", force: :cascade do |t|
+  end
 
 end
